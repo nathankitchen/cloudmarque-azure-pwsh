06162e9f6c20e81d408a8a5d76b0371793020b9d
Deepayan Pandharkar <Deepayan.Pandharkar@Trustmarque.com>
2020-07-02 14:49:51 +0000

    adding test case for clear-cmazcontext

--- Tests/Test-CmAzContext.Tests.ps1
+++ Tests/Test-CmAzContext.Tests.ps1
@@ -4,53 +4,59 @@ $projectRoot = "$PSScriptRoot\..\Cloudmarque.Azure\Resources\Project";
 $testProjectRoot = "$PSScriptRoot\TestProject"
 
 Describe "*-CmAzContext Lifecycle Tests" {
-  Context "Get-CmAzContext" {
-    It "Should fail when no CmAzContext is available and -ThrowIfUnavailable is set" {
-      {
-        Clear-CmAzContext
-        Get-CmAzContext -ThrowIfUnavailable
-      } | Should Throw "No Cloudmarque Azure context is in place, please run Set-CmAzContext before running this command."
-    }
-    It "Should fail when Azure is unavailable and -RequireAzure is set" {
-      {
-        Clear-AzContext -Force -Scope "Process"
-        Set-CmAzContext -ProjectRoot $projectRoot -Environment "Development"
-        Get-CmAzContext -ThrowIfUnavailable -RequireAzure
-      } | Should Throw "You must be logged into Azure. Please ensure you have an AzContext before running this command."
-    }
-    It "Should get the current context" {
-      Clear-AzContext -Force
-      Set-CmAzContext -ProjectRoot $projectRoot -Environment "Development"
-      Set-CmAzContext -ProjectRoot $projectRoot -Environment "Test"
-      $context = Get-CmAzContext -ThrowIfUnavailable
-      $context.Environment | Should -Be "Test"
-    }
-  }
-  Context "Set-CmAzContext" {
-    It "Should return the newly configured context" {
-      $c1 = Set-CmAzContext -ProjectRoot $projectRoot -Environment "Development"
-      $c2 = Set-CmAzContext -ProjectRoot $projectRoot -Environment "Test"
-      $c1.Environment | Should -Be "Development"
-      $c2.Environment | Should -Be "Test"
-    }
-    It "Should auto create a project root if confirmation parameter is y" {
-      Set-CmAzContext -ProjectRoot $testProjectRoot -Environment "Development" -ProjectConfirmation "y"
-      $expectedItems = Get-ChildItem $projectRoot -Recurse -Force
-      $actualItems = Get-ChildItem $testProjectRoot -Recurse -Force
-      $differences = Compare-Object $expectedItems $actualItems -Property Name, Length
-      $differences | should Be $null
-    }
-  }
-  Context "Clear-CmAzContext" {
-    It "Should make the current context null" {
-      $c1 = Set-CmAzContext -ProjectRoot $projectRoot -Environment "Development"
-      Clear-CmAzContext
-      $c2 = Get-CmAzContext
-      $c1.Environment | Should -Be "Development"
-      $c2 | Should -Be $Null
-    }
-  }
-  AfterAll {
-    Remove-Item -path $testProjectRoot -Recurse -Force
-  }
+	Context "Get-CmAzContext" {
+		It "Should fail when no CmAzContext is available and -ThrowIfUnavailable is set" {
+			{
+				Clear-CmAzContext
+				Get-CmAzContext -ThrowIfUnavailable
+			} | Should Throw "No Cloudmarque Azure context is in place, please run Set-CmAzContext before running this command."
+		}
+		It "Should clear profile on clearing CmAzContext" {
+			$value = "`$global:CMAZ_CTX = New-Object PSObject -Property  @{Environment = `"$($CMAZ_CTX.Environment)`"; ProjectRoot = `"$($CMAZ_CTX.ProjectRoot)`"; BuildId= `"$($CMAZ_CTX.BuildId)`"}"
+			Clear-CmAzContext
+			$filecontent = (Get-content $PROFILE.CurrentUserCurrentHost -Erroraction SilentlyContinue) -like $value
+			$filecontent | Should -Be $Null
+		}
+		It "Should fail when Azure is unavailable and -RequireAzure is set" {
+			{
+				Clear-AzContext -Force -Scope "Process"
+				Set-CmAzContext -ProjectRoot $projectRoot -Environment "Development"
+				Get-CmAzContext -ThrowIfUnavailable -RequireAzure
+			} | Should Throw "You must be logged into Azure. Please ensure you have an AzContext before running this command."
+		}
+		It "Should get the current context" {
+			Clear-AzContext -Force
+			Set-CmAzContext -ProjectRoot $projectRoot -Environment "Development"
+			Set-CmAzContext -ProjectRoot $projectRoot -Environment "Test"
+			$context = Get-CmAzContext -ThrowIfUnavailable
+			$context.Environment | Should -Be "Test"
+		}
+	}
+	Context "Set-CmAzContext" {
+		It "Should return the newly configured context" {
+			$c1 = Set-CmAzContext -ProjectRoot $projectRoot -Environment "Development"
+			$c2 = Set-CmAzContext -ProjectRoot $projectRoot -Environment "Test"
+			$c1.Environment | Should -Be "Development"
+			$c2.Environment | Should -Be "Test"
+		}
+		It "Should auto create a project root if confirmation parameter is y" {
+			Set-CmAzContext -ProjectRoot $testProjectRoot -Environment "Development" -ProjectConfirmation "y"
+			$expectedItems = Get-ChildItem $projectRoot -Recurse -Force
+			$actualItems = Get-ChildItem $testProjectRoot -Recurse -Force
+			$differences = Compare-Object $expectedItems $actualItems -Property Name, Length
+			$differences | should Be $null
+		}
+	}
+	Context "Clear-CmAzContext" {
+		It "Should make the current context null" {
+			$c1 = Set-CmAzContext -ProjectRoot $projectRoot -Environment "Development"
+			Clear-CmAzContext
+			$c2 = Get-CmAzContext
+			$c1.Environment | Should -Be "Development"
+			$c2 | Should -Be $Null
+		}
+	}
+	AfterAll {
+		Remove-Item -path $testProjectRoot -Recurse -Force
+	}
 }
