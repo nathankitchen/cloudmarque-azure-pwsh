bba37cf62b8044ef8d7db511294fddb3d63e8e3f
Deepayan Pandharkar <Deepayan.Pandharkar@Trustmarque.com>
2020-07-02 14:24:58 +0000

    reset profile

--- Cloudmarque.Azure/Public/Common/Clear-CmAzContext.ps1
+++ Cloudmarque.Azure/Public/Common/Clear-CmAzContext.ps1
@@ -9,17 +9,27 @@
 		 preventing further context-dependent commands from executing.
 
    .Example
-    # Clear the current Cloudmarque.Azure context
-    Clear-CmAzContext
+	# Clear the current Cloudmarque.Azure context
+	Clear-CmAzContext
   #>
-  [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidGlobalVars", "", Justification="Using a global variable for session state")]
-  [CmdletBinding()]
-  param()
-  process {
-    if ($global:CMAZ_CTX) {
-      Remove-Variable "CMAZ_CTX" -Scope "Global"
-    }
+	[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidGlobalVars", "", Justification = "Using a global variable for session state")]
+	[CmdletBinding()]
+	param()
+	process {
+		if ($(test-path $PROFILE.CurrentUserCurrentHost)) {
+			$value = "`$global:CMAZ_CTX = New-Object PSObject -Property  @{Environment = `"$($CMAZ_CTX.Environment)`"; ProjectRoot = `"$($CMAZ_CTX.ProjectRoot)`"; BuildId= `"$($CMAZ_CTX.BuildId)`"}"
+			$filecontent = (Get-content $PROFILE.CurrentUserCurrentHost) -notlike $value
 
-    Write-Verbose "Cloudmarque.Azure context cleared successfully"
-  }
+			if ($filecontent -eq "False" -or !$filecontent) {
+				Remove-Item $PROFILE.CurrentUserCurrentHost
+			}
+			else {
+				$filecontent | Out-File $PROFILE.CurrentUserCurrentHost
+			}
+		}
+		if ($global:CMAZ_CTX) {
+			Remove-Variable "CMAZ_CTX" -Scope "Global"
+		}
+		Write-Verbose "Cloudmarque.Azure context cleared successfully"
+	}
 }
