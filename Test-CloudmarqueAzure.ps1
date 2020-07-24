6b4aa80769f9a47cce13981eea68c90e8b5cdb3e
tm-luke <65401878+tm-luke@users.noreply.github.com>
2020-07-24 13:43:17 +0000

    add debugpreference to test run

--- Test-CloudmarqueAzure.ps1
+++ Test-CloudmarqueAzure.ps1
@@ -19,20 +19,20 @@
         [String]
         $Scope = "Auto"
     )
-    Process {
+    
+    $DebugPreference = "SilentlyContinue"
 
-        $modules =  @(
-            @{ Name = "Pester"; Version = "4.10.1" },
-            @{ Name = "PSScriptAnalyzer"; Version = "1.18.3" }
-        );
+    $modules =  @(
+        @{ Name = "Pester"; Version = "4.10.1" },
+        @{ Name = "PSScriptAnalyzer"; Version = "1.18.3" }
+    );
 
-        . ./Install-Dependencies.ps1
+    . ./Install-Dependencies.ps1
 
-        Install-Dependencies -AdditionalModules $modules -Scope $Scope -Verbose
+    Install-Dependencies -AdditionalModules $modules -Scope $Scope -Verbose
 
-        New-Item -Path "$PSScriptRoot\publish\" -Name $Project -ItemType Directory -Force | Out-Null
-        New-Item -Path "$PSScriptRoot\publish\tests\" -Name $Project -ItemType Directory -Force | Out-Null
+    New-Item -Path "$PSScriptRoot\publish\" -Name $Project -ItemType Directory -Force | Out-Null
+    New-Item -Path "$PSScriptRoot\publish\tests\" -Name $Project -ItemType Directory -Force | Out-Null
 
-        Invoke-Pester -Script ".\Tests\*" -OutputFile "$PSScriptRoot\publish\tests\nunit-results.xml" -OutputFormat 'NUnitXML'
-    }
+    Invoke-Pester -Script ".\Tests\*" -OutputFile "$PSScriptRoot\publish\tests\nunit-results.xml" -OutputFormat 'NUnitXML' -Force
 }
